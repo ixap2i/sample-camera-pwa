91216e2e2a3de6f2b427147b49c7177fc11dbc57
ixap2i <ayamashita124@gmail.com>
2021-09-02 15:06:05 +0900

    pwa init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+};
